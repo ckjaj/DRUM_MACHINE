521f8ccb2444382fcbbf9b3286601b5dc852b56b
jackhetz <jackhetz7@gmail.com>
2026-02-28 21:54:09 -0500

    testing why buttons are pressing too much

--- test1.py
+++ test1.py
@@ -2,15 +2,19 @@ from gpiozero import Button
 from signal import pause
 
 button1 = Button(22)
+button1.name = "1"
 button2 = Button(27)
+button2.name = "2"
 button3 = Button(23)
+button3.name = "3"
 button4 = Button(24)
+button4.name = "4"
 
-def but_pres():
-	print(f"Button was pressed")
+def but_pres(b):
+	print(f"Button {b.name} was pressed")
 
-def but_rele():
-	print(f"Button was pressed")
+def but_rele(b):
+	print(f"Button {b.name} was released")
 
 button1.when_pressed = but_pres
 button1.when_released = but_rele
