7a29ace4aae95539415ee6ba84dde228a2640cfb
jackhetz <jackhetz7@gmail.com>
2026-02-28 22:08:08 -0500

    for loop

--- test1.py
+++ test1.py
@@ -1,32 +1,15 @@
 from gpiozero import Button
 from signal import pause
 
-button1 = Button(22)
-button1.name = "1"
-button2 = Button(27)
-button2.name = "2"
-button3 = Button(23)
-button3.name = "3"
-button4 = Button(24)
-button4.name = "4"
+pins = [22, 27, 23, 24]
 
-def but_pres(b):
-	print(f"Button {b.name} was pressed")
+names = {}
 
-def but_rele(b):
-	print(f"Button {b.name} was released")
-
-button1.when_pressed = but_pres
-button1.when_released = but_rele
-
-button2.when_pressed = but_pres
-button2.when_released = but_rele
-
-button3.when_pressed = but_pres
-button3.when_released = but_rele
-
-button4.when_pressed = but_pres
-button4.when_released = but_rele
+for i, pin in enumerate(pins):
+	but = Button(pin)
+	names[pin] = str(i+1)
+	but.when_pressed = lambda b: print(f"button {names[b.pin.number]} pressed")
+	but.when_released = lambda b: print(f"button {names[b.pin.number]} released")
 
 
 pause()
