749e5b7ca952b41d8066cc2c88307af078f7fdca
jackhetz <jackhetz7@gmail.com>
2026-02-28 21:07:13 -0500

    add button test

---
+++ test1.py
@@ -0,0 +1,21 @@
+from gpiozero import Button
+from signal import pause
+
+button1 = Button(0)
+button2 = Button(1)
+button3 = Button(2)
+button4 = Button(3)
+
+button1.when_pressed = lambda b: print(f"Button1 was pressed")
+button1.when_released = lambda b: print(f"Button1 was released")
+
+button2.when_pressed = lambda b: print(f"Button2 was pressed")
+button2.when_released = lambda b: print(f"Button2 was released")
+
+button3.when_pressed = lambda b: print(f"Button3 was pressed")
+button3.when_released = lambda b: print(f"Button3 was released")
+
+button4.when_pressed = lambda b: print(f"Button4 was pressed")
+button4.when_released = lambda b: print(f"Button4 was released")
+
+
