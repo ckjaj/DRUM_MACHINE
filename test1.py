385db5fdc78425aecadcedac764c0b74142bf6b7
jackhetz <jackhetz7@gmail.com>
2026-02-28 23:10:11 -0500

    added led's

--- test1.py
+++ test1.py
@@ -1,16 +1,33 @@
 from gpiozero import Button
 from signal import pause
+from gpiozero import LED
+
 
 pins = [22, 27, 23, 24]
 
-names = {}
+pins_led = [26, 16, 5, 6]
 
 for i, pin in enumerate(pins):
 	but = Button(pin)
-	names[pin] = str(i+1)
-	but.when_pressed = lambda b: print(f"button {names[b.pin.number]} pressed")
-	but.when_released = lambda b: print(f"button {names[b.pin.number]} released")
+	led = LED(pins_led[i])
+
+	
+	def make_pressed(led_ref, name):
+		def pressed(led_ref, name):
+			print(f"button {name} pressed")
+			led_ref.on()
+		return pressed
+
+	def make_released(led_ref, name):
+		def released(led_ref, name):
+			print(f"button {name} released")
+			led_ref.off()
+		return released
+
+	but.when_pressed = make_pressed(led_ref, str(i+1))
+	but.when_released = make_released(led_ref, str(i+1))
 	print(f"button @ pin {pin} finished initialized")
+
 
 
 pause()
