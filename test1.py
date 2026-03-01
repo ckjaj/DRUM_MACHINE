76d90667803f3960086963bb9e0ef16d91dfe500
jackhetz <jackhetz7@gmail.com>
2026-02-28 21:33:29 -0500

    add button test

--- test1.py
+++ test1.py
@@ -6,17 +6,23 @@ button2 = Button(1)
 button3 = Button(2)
 button4 = Button(3)
 
-button1.when_pressed = lambda: print(f"Button1 was pressed")
-button1.when_released = lambda: print(f"Button1 was released")
+def but_pres():
+	print(f"Button1 was pressed")
 
-button2.when_pressed = lambda: print(f"Button2 was pressed")
-button2.when_released = lambda: print(f"Button2 was released")
+def but_rele():
+	print(f"Button1 was pressed")
 
-button3.when_pressed = lambda: print(f"Button3 was pressed")
-button3.when_released = lambda: print(f"Button3 was released")
+button1.when_pressed = but_pres
+button1.when_released = but_rele
 
-button4.when_pressed = lambda: print(f"Button4 was pressed")
-button4.when_released = lambda: print(f"Button4 was released")
+button2.when_pressed = but_pres
+button2.when_released = but_rele
+
+button3.when_pressed = but_pres
+button3.when_released = but_rele
+
+button4.when_pressed = but_pres
+button4.when_released = but_rele
 
 
 pause()
